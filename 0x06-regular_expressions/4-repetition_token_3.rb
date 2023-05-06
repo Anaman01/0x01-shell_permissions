8f1573e0f0171cd7cefa6b595d37e7417c9b8a47
Anaman01 <eanaman16@gmail.com>
2023-05-05 19:24:47 -0700

    expressions

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# A Ruby script that accepts one argument and pass it to a regular expression matching metho
+# A regular expression that is matches a given pattern
 puts ARGV[0].scan(/hbt*n/).join
